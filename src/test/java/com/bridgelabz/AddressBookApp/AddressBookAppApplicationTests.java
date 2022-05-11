5737f8f0452e8b5c582a92821a18cacbb66a78ab
tomthomas1 <tomthomas8688@gmail.com>
2022-05-11 22:30:10 +0530

    S1-UC1 : Created address book project setup

---
+++ src/test/java/com/bridgelabz/AddressBookApp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.AddressBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
